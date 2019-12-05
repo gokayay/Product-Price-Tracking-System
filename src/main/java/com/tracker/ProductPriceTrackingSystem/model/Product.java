f5d4ae053c875a101041fcdf7d1a9174019cf4ca
Gokay Ay <goay@Gokays-MacBook-Pro.local>
2019-12-05 17:29:30 +0300

    crud-op-bulut

---
+++ src/main/java/com/tracker/ProductPriceTrackingSystem/model/Product.java
@@ -0,0 +1,51 @@
+package com.tracker.ProductPriceTrackingSystem.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = false)
+    private String productName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductComment() {
+        return productComment;
+    }
+
+    public void setProductComment(String productComment) {
+        this.productComment = productComment;
+    }
+
+    @Column
+    private String productComment;
+
+    public Product() {
+    }
+}
