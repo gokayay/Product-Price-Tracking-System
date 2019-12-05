2c2faa6b9f980c640f7b8cbb32f2409a21741693
gokayay <gokayay98@gmail.com>
2019-12-05 11:04:03 +0300

    initial-commit

---
+++ src/main/java/com/tracker/ProductPriceTrackingSystem/ProductPriceTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.ProductPriceTrackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPriceTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPriceTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracker/ProductPriceTrackingSystem/ProductPriceTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.ProductPriceTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPriceTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
