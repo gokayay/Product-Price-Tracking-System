f57f2f166274ea31807532aadf7f84ff8272274c
Gokay Ay <goay@Gokays-MacBook-Pro.local>
2019-12-11 11:28:43 +0300

    starting-for-postgresql

--- src/main/java/com/tracker/ProductPriceTrackingSystem/ProductPriceTrackingSystemApplication.java
+++ src/main/java/com/tracker/ProductPriceTrackingSystem/ProductPriceTrackingSystemApplication.java
@@ -9,5 +9,5 @@ public class ProductPriceTrackingSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductPriceTrackingSystemApplication.class, args);
 	}
-
+	//Control for branch
 }
