e6cb888748e50813c4ac2a574b4e26fea8dc5a09
Gokay Ay <goay@Gokays-MacBook-Pro.local>
2020-01-21 09:24:40 +0300

    added-imageUrlColumn-toDb

--- src/main/java/com/tracker/ProductPriceTrackingSystem/scheduler/TimerExample.java
+++ src/main/java/com/tracker/ProductPriceTrackingSystem/scheduler/TimerExample.java
@@ -15,7 +15,7 @@ public class TimerExample {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
 
-    @Scheduled(fixedRate = 300000)   // 5 minutes
+    @Scheduled(fixedRate = 1800000)   // 5 hours
     public void reportCurrentTime() {
 
         log.info("The time is now {}", dateFormat.format(new Date()));
